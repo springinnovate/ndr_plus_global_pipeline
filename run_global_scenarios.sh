95afa6f82b0c70edd741606b49954a8fd87dbc1a
Rich Sharp <richpsharp@gmail.com>
2021-03-26 18:41:57 -0700

    do the run and good name

--- run_global_scenarios.sh
+++ run_global_scenarios.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for scenario_id in extensification_bmps_irrigated extensification_bmps_rainfed extensification_current_practices extensification_intensified_irrigated extensification_intensified_rainfed fixedarea_bmps_irrigated fixedarea_bmps_rainfed fixedarea_intensified_irrigated fixedarea_intensified_rainfed grazing_expansion restoration sustainable_currentpractices
+for scenario_id in restoration sustainable_currentpractices
 do
-    echo docker run -it --rm -v `pwd`:/usr/local/workspace --name ndr --shm-size=32gb therealspring/inspring:latest global_ndr_plus_pipeline.py scenarios.nci_global --limit_to_scenarios $scenario_id
+    docker run -it --rm -v `pwd`:/usr/local/workspace --name ndr_$scenario_id --shm-size=32gb therealspring/inspring:latest global_ndr_plus_pipeline.py scenarios.nci_global --limit_to_scenarios $scenario_id
 done

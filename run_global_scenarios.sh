5070ec686b21bdfcc5fd12d524e268195fb85243
Rich Sharp <richpsharp@gmail.com>
2021-03-28 14:03:58 -0700

    adding sustaniable current and fii

--- run_global_scenarios.sh
+++ run_global_scenarios.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for scenario_id in restoration grazing_expansion sustainable_currentpractices
+for scenario_id in sustainable_current fixedarea_intensified_irrigated
 do
     docker run -it --rm -v `pwd`:/usr/local/workspace --name ndr_$scenario_id --shm-size=32gb therealspring/inspring:latest global_ndr_plus_pipeline.py scenarios.nci_global --limit_to_scenarios $scenario_id
 done

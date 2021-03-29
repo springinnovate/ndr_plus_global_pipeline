8f508cd6de42f4c27206983a2b00db09e281b5ac
beckyck <59622808+beckyck@users.noreply.github.com>
2021-03-29 16:08:06 -0700

    Running just fixedarea_intensified_rainfed

--- run_global_scenarios.sh
+++ run_global_scenarios.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for scenario_id in sustainable_current fixedarea_intensified_irrigated
+for scenario_id in fixedarea_intensified_rainfed
 do
     docker run -it --rm -v `pwd`:/usr/local/workspace --name ndr_$scenario_id --shm-size=32gb therealspring/inspring:latest global_ndr_plus_pipeline.py scenarios.nci_global --limit_to_scenarios $scenario_id
 done
